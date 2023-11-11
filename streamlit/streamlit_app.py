ee1a8b8c3f851c91097f76136e091cdecac60aba
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2023-11-11 19:58:42 +0100

    select an image done

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,42 +4,67 @@ import math
 import pandas as pd
 import streamlit as st
 from PIL import Image
+import os
 
+
+def print_images(
+    cols,
+    images_ids,
+    start,
+    size
+):
+    images = list(
+        map(
+            lambda id: Image.open(f"{images_path}{id}"),
+            images_ids[start:(start + size):1],
+        )
+    )
+    n = 0
+    for col in cols:
+        i = n
+        while i < 15:
+            with col:
+                st.image(images[i])
+            i = i + 5
+        n = n + 1
 
 """
-# Welcome to Streamlit!
+# Welcome to Mango!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Choosse a product to generate an outfit:
 """
-resource_path = "../resources/"
 
 
-image = Image.open(f'{resource_path}icon.png')
+cols = st.columns([1, 1, 1, 1, 1])
+images_path = "../data/images/"
 
-st.image(image, caption='Outfit')
+images_ids = os.listdir(f"{images_path}")
+# images_generated = []
+# images_generated_ids = [
+#     "2019_41005828_05",
+#     "2019_41017020_08",
+#     "2019_41025020_02",
+#     "2019_41025782_23",
+#     "2024_67050279_CO",
+# ]
+# for image_id in images_generated_ids:
+#     images_generated.append(Image.open(f"{images_path}{image_id}.jpg"))
+size = 15
+if 'start' not in st.session_state.keys():
+    st.session_state['start'] = 0
+if st.button(
+    "Next Page ->",
+    type="primary",
+):
+    st.session_state['start'] = st.session_state['start']+size
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+print_images(cols, images_ids, st.session_state['start'], size)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# st.button("Generate", type="primary")
+# for col in cols:
+#     i = n
+#     while i < len(images_generated):
+#         with col:
+#             st.image(images_generated[i], width=100)
+#         i = i + 5
+#     n = n + 1
